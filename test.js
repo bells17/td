6057c25424682c70ee75009eb4cd7be77ec56d8c
bells17 <bells171@gmail.com>
2016-09-16 19:27:19 +0900

    Add test case

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
-const tdf = require("./index");
+const fs = require("fs");
+const co = require("co");
 const assert = require("power-assert");
+const tdf = require("./index");
+
 
 describe("tdf", () => {
-  describe("sample", () => {
-    it("sample", () => {
-      assert(true);
+  describe("flename", () => {
+    it("transform to csv from json", (done) => {
+      co(function*() {
+        const filepath = "./sample/sample";
+        const transformed = yield tdf(`${filepath}.json`, {}, true);
+        const csv = fs.readFileSync(`${filepath}.csv`, "UTF8");
+        assert(transformed === csv);
+        done();
+      });
     });
   });
 });
